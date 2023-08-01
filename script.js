a57e5c3b0ba433f3d8b11e81078fcd5d8590191b
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2023-08-01 07:45:35 +0200

    round player function implementation

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie! You both chose " + playerSelection;
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        return "You win! " + playerSelection + " beats " + computerSelection;
+    } else {
+        return "You lose! " + computerSelection + " beats " + playerSelection;
+    }
+}
